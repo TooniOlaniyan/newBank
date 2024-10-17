59fb86a4fe16832f62da3573ae8704fd61d41405
unknown <adesiyantope2014@gmail.com>
2024-10-17 17:14:39 +0100

    data

--- pages/api/part.js
+++ pages/api/part.js
@@ -21,6 +21,6 @@ export default async function handler(req, res) {
     
     res.status(200).json(response.data);
   } catch (error) {
-    res.status(500).json({ error: `${error}` });
+    res.status(500).json({ error: `${error}` })
   }
 }
